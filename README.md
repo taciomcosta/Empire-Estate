aa523e7f0e6ecbed9c964425d2b0d3bf20cb888a
Tacio <taciomcosta@gmail.com>
2017-09-06 21:25:09 -0300

    Empire-State added

--- README.md
+++
@@ -1,7 +0,0 @@
-﻿# Empire-Estate
-Projeto da disciplina Programação Web - 2° Período - 2016
-
--- Integrantes --
-Luiz Wanderley Campos Aguiar Junior,
-Mateus da Silva Freitas,
-Tácio Monteiro Costa
--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//Conectando ao mysql
-	$conn = mysqli_connect('localhost','root','1234567','empireestate') or die ("Erro ao se conectar ao banco de dados.");
-	//Define o encoding que virá do cliente, para ser usado no mysql
-	mysqli_set_charset($conn, 'utf8');
-
-?>
